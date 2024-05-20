306050d038ba8949462e41b587c97c03d9e4a599
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-05-20 14:26:48 -0700

    Create basic header links

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,9 +3,24 @@ import styles from "./Header.module.css";
 const Header = () => {
   return (
     <>
-      <h1 className={styles.blue}>Shopping cart app header</h1>
+      <header>
+        <h1 className={styles.blue}>Shopping cart app header</h1>
+        <nav >
+            <ul className = {styles.navbar}>
+                <li>
+                    <a href="home">Home</a>
+                </li>
+                <li>
+                    <a href="products">Products</a>
+                </li>
+                <li>
+                    <a href="shoppingCart">Shopping Cart</a>
+                </li>
+            </ul>
+        </nav>
+      </header>
     </>
   );
 };
 
-export default Header
+export default Header;
