fb743eec75d3b1c2d85c639548f22e9751448ada
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-05-20 14:06:19 -0700

    Create component directory and header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 
+import Header from './components/header/Header.jsx'
+
 function App() {
 
   return (
     <>
-      <h1>Shopping Cart</h1>
-    
+    <Header/>
     </>
   )
 }
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+import styles from "./Header.module.css";
+
+const Header = () => {
+  return (
+    <>
+      <h1 className={styles.blue}>Shopping cart app header</h1>
+    </>
+  );
+};
+
+export default Header
