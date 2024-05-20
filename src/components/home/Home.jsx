38a3462049ef582f902188f3d95ba035f2a1ea95
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-05-20 16:30:49 -0700

    Add photo to home section

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,11 +1,22 @@
 import Header from "../header/Header";
+import styles from "./Home.module.css";
+import lindsayAndAlbie from "../../images/lindsayAndAlbie.jpeg";
 
 const Home = () => {
   return (
     <>
       <Header />
       <section>
-        <h3>You're in the home sction</h3>
+        <h3>You're in the home section.</h3>
+
+        <div className={styles.imgContainer}>
+          <img
+            src={lindsayAndAlbie}
+            alt="Lovely lady with a large handsome dog."
+          ></img>
+        </div>
+
+        <h4>Doesn't this look like home??</h4>
       </section>
     </>
   );
