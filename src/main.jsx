2bbae349b1570b65a5caf0de158c2d271abdcee7
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-05-22 13:30:35 -0700

    Utilize imported route object

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import App from "./App.jsx";
-import Header from "./components/header/Header.jsx";
-import Home from "./components/home/Home.jsx";
-import Products from "./components/products/Products.jsx";
-import Cart from "./components/cart/Cart.jsx";
-
+import routes from "./routes.jsx";
 import "./index.css";
 
-// Tried to call a useState hook here and pass the state variable as a prop to products, doesn't work. Invalid hook call.
 
-const router = createBrowserRouter([
-  { path: "/", element: <App /> },
-  {
-    path: "products",
-    element: <Products />,
-  },
-  {
-    path: "shoppingCart",
-    element: <Cart />,
-  },
-]);
+const router = createBrowserRouter(routes)
+
+
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,11 +1,12 @@
 import Cart from "./components/cart/Cart";
 import Products from "./components/products/Products";
 import Home from "./components/home/Home";
+import App from "./App";
 
 const routes = [
   {
     path: "/",
-    element: <Home />,
+    element: <App />,
     children: [
       { path: "/products", element: <Products /> },
       { path: "shoppingCart", element: <Cart /> },
@@ -15,3 +16,9 @@ const routes = [
 
 
 export default routes
+
+//  *********************************************************************************************************
+
+// Our App will return Header and handle top level state. Child components will be products and shopping cart. 
+// The "Home" content will be in it's own file but it won't be a new route. Just the component that's displayed on the index route
+//  *********************************************************************************************************
