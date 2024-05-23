ad27c8ecdc7cd5b7f229c6075e33d9e31a24cd0d
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-05-23 14:44:11 -0700

    Format product card grid

--- src/components/productCard/Card.jsx
+++ src/components/productCard/Card.jsx
@@ -3,14 +3,19 @@ import styles from "./Card.module.css";
 const Card = ({ product }) => {
   // These are the individual product cards
 
+
+// We will need an onsubmit handler for the button
+
+
   return (
     <div className={styles.productCard}>
       <img src={product.image}></img>
-      <h3>{product.title}</h3>
-      <h2>{product.price}</h2>
+      <h3 className={styles.title}>{product.title}</h3>
+      <h2 className={styles.price}>{product.price}</h2>
       <img></img>
       <form>
-        <input type="number"></input>
+        <input type="number" value="1"></input>
+        <button type="submit">Add to cart</button>
       </form>
     </div>
   );
