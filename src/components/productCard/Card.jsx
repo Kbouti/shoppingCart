d2558bb38bc8a2dc7252465d9f2b159d4bc332a3
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-05-24 12:21:39 -0700

    Set new shopping cart state and comment next steps

--- src/components/productCard/Card.jsx
+++ src/components/productCard/Card.jsx
@@ -1,11 +1,70 @@
 import styles from "./Card.module.css";
 
-const Card = ({ product }) => {
+const Card = ({ product, shoppingCart, setShoppingCart }) => {
   // These are the individual product cards
 
+  // We will need an onsubmit handler for the button
 
-// We will need an onsubmit handler for the button
+  const addToCart = (e) => {
+    e.preventDefault();
+    console.log(`triggered addToCart for ${product.title} quantity: `);
+    const quantity = e.target.children[0].value;
+    console.log(quantity);
+    // Now we need to update the shopping cart with this product.
+    // First we need to check if it's in the shopping cart already.
+    let newArray = [];
 
+//  ************************************************************************************************************************
+// This is our issue. Even once we've added the item and set new shoppingCart state, it never determines that yes the item is in the cart. 
+// Maybe we gotta delete the old entry and make a new one. 
+
+
+// SO: 
+// Loop through shoppingCart. 
+// If entry matches ours, store entries current quantity, then delete it. 
+// If no match, push to new array. 
+// Create new object. Quantity will be input qty plus saved qty 
+//  ************************************************************************************************************************
+    if (!shoppingCart.contains((item) => item.product.title == product.title)) {
+      console.log(`it's not in the cart`);
+
+      console.log(product.title);
+
+      console.log(`shoppingCart:`);
+      console.log(shoppingCart);
+
+      console.log(`shoppingCart.length:`);
+      console.log(shoppingCart.length);
+      for (let i = 0; i < shoppingCart.length; i++) {
+        console.log(shoppingCart[i].product.title);
+      }
+
+// Ok WHY isn't it getting to our else statment????
+
+
+      const newEntry = { product: product, quantity: quantity };
+
+      for (let i = 0; i < shoppingCart.length; i++) {
+        newArray.push(shoppingCart[i]);
+      }
+      newArray.push(newEntry);
+    } else {
+      console.log(`it is in the cart`);
+      for (let i = 0; i < shoppingCart.length; i++) {
+        if (shoppingCart[i].product.title !== product.title) {
+          newArray.push(shoppingCart[i]);
+        } else {
+          let newEntry = {
+            product: shoppingCart[i],
+            quantity: (shoppingCart[i].quantity += quantity),
+          };
+          newArray.push(newEntry);
+        }
+      }
+    }
+
+    setShoppingCart(newArray);
+  };
 
   return (
     <div className={styles.productCard}>
@@ -13,7 +72,7 @@ const Card = ({ product }) => {
       <h3 className={styles.title}>{product.title}</h3>
       <h2 className={styles.price}>${product.price}</h2>
       <img></img>
-      <form>
+      <form onSubmit={addToCart}>
         <input type="number" defaultValue="1"></input>
         <button type="submit">Add to cart</button>
       </form>
--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,15 +1,21 @@
 import Card from "../productCard/Card";
 import styles from "./Products.module.css";
 
-const Products = ({ apiData }) => {
-
+const Products = ({ apiData, shoppingCart, setShoppingCart }) => {
   return (
     <>
       <section className={styles.products}>
         <h3>Products</h3>
         <div className={styles.cardContainer}>
           {apiData.map((product) => {
-            return <Card key={product.id} product={product} />;
+            return (
+              <Card
+                key={product.id}
+                product={product}
+                shoppingCart={shoppingCart}
+                setShoppingCart={setShoppingCart}
+              />
+            );
           })}
         </div>
       </section>
