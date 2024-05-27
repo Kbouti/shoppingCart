20a17648b909e547edb286e4ffe90d04fb8aeb8b
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-05-27 11:46:58 -0700

    Create remove button

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,23 +1,31 @@
 import styles from "./Cart.module.css";
 
 const Cart = ({ shoppingCart, setShoppingCart }) => {
-  return (
+  const remove = (e) => {
+    // RemoveHandler to take item off shopping cart
+    e.preventDefault();
+    console.log(`remove button clicked`);
+  };
 
-      <section className={styles.cart}>
-        <h3>Shopping Cart</h3>
-        {shoppingCart.length < 1 ? <p>Your cart is empty</p> :
+  return (
+    <section className={styles.cart}>
+      <h3>Shopping Cart</h3>
+      {shoppingCart.length < 1 ? (
+        <p>Your cart is empty</p>
+      ) : (
         <ul>
           {shoppingCart.map((item) => {
             return (
               <li key={item.product.id}>
                 <p>{item.product.title}</p>
                 <p>quantity: {item.quantity}</p>
+                <button onClick={remove}>Remove</button>
               </li>
             );
           })}
-        </ul>}
-      </section>
-
+        </ul>
+      )}
+    </section>
   );
 };
 
