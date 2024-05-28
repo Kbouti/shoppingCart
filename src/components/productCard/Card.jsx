c975860db2f910feaa7ffabf69d94057ac78d27d
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-05-28 15:15:44 -0700

    Move button css to Card module

--- src/components/productCard/Card.jsx
+++ src/components/productCard/Card.jsx
@@ -6,9 +6,7 @@ import styles from "./Card.module.css";
 const Card = ({ product, shoppingCart, setShoppingCart }) => {
   const addToCart = (e) => {
     e.preventDefault();
-    console.log(`triggered addToCart for ${product.title} quantity: `);
     const quantity = e.target.children[0].value;
-    console.log(quantity);
     if (quantity < 1) {
       alert(`Must enter a valid quantity`);
       return;
@@ -19,9 +17,6 @@ const Card = ({ product, shoppingCart, setShoppingCart }) => {
     for (let i = 0; i < shoppingCart.length; i++) {
       if (shoppingCart[i].product.title === targetProduct) {
         priorQuantity = shoppingCart[i].quantity;
-        console.log(
-          `This item is already in the cart, adding ${quantity} to ${priorQuantity}`
-        );
       } else {
         const newEntry = {
           product: shoppingCart[i].product,
@@ -49,7 +44,7 @@ const Card = ({ product, shoppingCart, setShoppingCart }) => {
       <img></img>
       <form onSubmit={addToCart}>
         <input type="number" min={1} defaultValue={1}></input>
-        <button type="submit">Add to cart</button>
+        <button className={styles.resting} type="submit">Add to cart</button>
       </form>
     </div>
   );
