92091a14d80a83c91011363980ac08401d279474
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-05-28 10:48:20 -0700

    Attempt first test

--- src/tests/App.test.jsx
+++ src/tests/App.test.jsx
@@ -1,22 +1,11 @@
-import {describe, it, expect} from 'vitest';
-
-describe(`something truthy and falsy`, () => {
-    it(`true to be true`, () => {
-        expect(true).toBe(true);
-    });
-
-    it(`false to be false`, () => {
-        expect(false).toBe(false);
-    })
-})
-
 import {render, screen} from '@testing-library/react';
 import App from '../App';
 
 describe('App', () => {
-    it('renders headline', () => {
-        render(<App title="React" />);
-
-        screen.debug();
+    it('renders heading', () => {
+        render(<App />);
+        expect(screen.getByRole("heading").textContent).toContain("Kevdawg's webShop");
     })
 })
+
+// I'll have to go back and revisist testing, this doesn't pass. 
