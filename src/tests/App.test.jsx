74f8d5271f9a7fde4d94a857e45c30859b695f84
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-10-15 12:26:30 -0700

    Test presence of home title

--- src/tests/App.test.jsx
+++ src/tests/App.test.jsx
@@ -1,6 +1,7 @@
 import { describe, it, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
 import App from "../App";
+import Home from "../components/home/Home";
 
 describe("App", () => {
   it("At first renders loading message", () => {
@@ -11,41 +12,14 @@ describe("App", () => {
   // ******************************************************************************************************************************************************************
 // So far the above is our only successful test. We need to somehow utilize componentDidMount or something similar rather than using a setTimeout. The set timeout somehow makes every test pass
 
-  it("Calls fetchPromises function", () => {
-    expect(App.fetchPromises).toHaveBeenCalledTimes(1);
-    // ^^ Something like this is what we should be trying to test next. If we can get this to work we are on the right track.
+  it("renders home message", () => {
+    render(<Home />);
+    expect(screen.getByText("You're in the home section.").textContent).toMatch(/You're in the home section./i);
   })
 
-
-
-  it("Renders title in less than 5 seconds", () => {
-    render(<App />);
-    setTimeout(() => {
-      expect(screen.getByText("Kevin's webShop").textContent).toMatch(
-        /Kevin's webShop/i
-      );
-
-      // ******************************************************************************************************************************************************************
-      // This passes even when it shouldn't too..... I wonder if there's an issue with using setTimeout
-      // Ok, yeah. Our loading test searchForWorkspaceRoot, we can make it pass or findAllByAltText. These tests after the timeout are all passing no matter what
-      // ******************************************************************************************************************************************************************
-    }, 5000);
-  });
-
-  it("renders addToCart button", () => {
-    render(<App />);
-
-    setTimeout(() => {
-      expect(screen.getByRole("addToCart").toBeInTheDocument());
-      // This returns true no matter what the role is defined as
-      // This test does not work, it passes no matter what. Tried with getByRole, getByText, doesn't matter. Is it because of setTimeout?
-
-      // const button = screen.getByRole("addToCart");
-      // expect(button.toBeInTheDocument());
-      // This test passes even when the addToCart button is not displayed..... BAD test
-    }, 5000);
-  });
 });
+
+
 
 describe("something truthy", () => {
   it("multiplies 2 x 2 = 4", () => {
